9ad0a9f4f4115ecc23819f7e03aad4cdf43f85a6
Parth Patel <parth6796@yahoo.com>
2021-08-18 10:23:16 +0100

    schema added

---
+++ setup.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS person;
+CREATE TABLE person(
+    personid INT SERIAL NOT NULL UNIQUE,
+    fname VARCHAR(20) NOT NULL,
+    sname VARCHAR(20) NOT NULL,
+    age INT CHECK(0<=age<=120),
+    housemates INT,
+    addressid INT NOT NULL,
+    PRIMARY KEY (personid)
+);
+DROP TABLE IF EXISTS house;
+CREATE TABLE house(
+    addressid INT NOT NULL UNIQUE,
+    ownername VARCHAR(40) NOT NULL
+);
+DROP TABLE IF EXISTS houseAddress;
+CREATE TABLE IF EXISTS houseAddress(
+    addressid INT SERIAL NOT NULL UNIQUE,
+    streetAddress VARCHAR(50) NOT NULL,
+    postcode VARCHAR(9) NOT NULL,
+    PRIMARY KEY (addressid) 
+);
